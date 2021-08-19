6d9d9becb47bef98596fa785f9d08e6fff05fef3
kirstie <kirstie@sightsource.net>
2021-08-19 03:57:38 -0400

    fireworks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.scss";
+import Timer from "./Timer.js";
+import Fireworks3 from "./Fireworks";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [countdown, setCountdown] = useState(false);
+return (
+   <div className="container">
+      {countdown ? <Fireworks3 /> : null}
+      <Timer setCountdown={setCountdown} />
+   </div>
+ );
+
 }
 
 export default App;
---
+++ src/Timer.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect} from 'react';
+import "./App.scss";
+
+function Timer({ setCountdown }) {
+   const [seconds, setSeconds] = useState(60);
+   const [isRunning, setIsRunning] = useState(false);
+
+   useEffect(() => {
+      if (isRunning) {
+         const id = window.setInterval(() => {
+            setSeconds(seconds => seconds -1)}, 1000);
+            return () => window.clearInterval(id);
+      }
+   }, [isRunning]);
+
+      if (seconds < 0) {
+         setSeconds(0);
+         setCountdown(true);
+         setTimeout(() => {
+            setCountdown(false)
+         }, 15000);
+      }
+
+
+   return (
+      <div className="app">
+         <div className='time-circle'>
+            <div className="time">
+               {seconds}
+            </div>
+         </div>
+         <div className="buttons">
+         <button className="play-pause" onClick={() => {
+            setIsRunning(true);
+         }}>
+            <i className="fa fa-play fa-2x" />
+         </button>
+         <button
+            disabled={!isRunning} 
+            className="reset" 
+            onClick={() => {
+               setIsRunning(true);
+               setSeconds(60);
+         }}>
+              Reset
+         </button>
+         </div>
+      </div>
+   )
+   }
+
+export default Timer
